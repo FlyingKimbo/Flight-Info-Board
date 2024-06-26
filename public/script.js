6a19ffc6d153c8b664e77dcc239f04ad23ac6f58
lucastng <lucastng@hotmail.com>
2024-06-27 03:34:06 +0800

    Automated update of repository

--- public/script.js
+++ public/script.js
@@ -65,26 +65,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 
-   
+    
 
-    async function fetchFlightData() {
-        try {
-            const response = await fetch('/api/update-flight');
-            if (response.ok) {
-                const data = await response.json();
-                console.log('Received data:', data);
-                document.getElementById('flight-data').textContent = JSON.stringify(data, null, 2);
-            } else {
-                document.getElementById('flight-data').textContent = 'Error fetching data';
-            }
-        } catch (error) {
-            document.getElementById('flight-data').textContent = 'Fetch error: ' + error.message;
-        }
-    }
-
-
-    // Fetch data every 5 seconds
-    setInterval(fetchFlightData, 5000);
 
 
   
@@ -401,8 +383,7 @@ document.addEventListener("DOMContentLoaded", function () {
             sortTable(3, 'asc');
         }
 
-        // Initial fetch
-        fetchFlightData();
+        
 
         // Fetch initial ETE value
         fetchInitialETE();
