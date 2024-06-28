23639307eaa321644a5be9139c07f1f6a405e687
lucastng <lucastng@hotmail.com>
2024-06-29 01:53:41 +0800

    Automated update of repository

--- api/saved-flight-state.js
+++ api/saved-flight-state.js
@@ -1,23 +1,11 @@
+// /api/saved-flight-state.js
+
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const app = express();
 
-app.use(express.json());
-
 const flightStateFilePath = path.join(__dirname, 'flight-state.json');
-
-app.post('/api/save-flight-state', (req, res) => {
-    const flightData = req.body.flightData;
-
-    fs.writeFile(flightStateFilePath, JSON.stringify(flightData), err => {
-        if (err) {
-            console.error('Error saving flight state:', err);
-            return res.status(500).send('Failed to save flight state');
-        }
-        res.send('Flight state saved successfully');
-    });
-});
 
 app.get('/api/saved-flight-state', (req, res) => {
     fs.readFile(flightStateFilePath, 'utf8', (err, data) => {
@@ -30,5 +18,6 @@ app.get('/api/saved-flight-state', (req, res) => {
 });
 
 module.exports = app;
+
 
 
