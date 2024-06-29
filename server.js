7df7788505d4f5cb5ca96f19b254671bf8f9c36f
lucastng <lucastng@hotmail.com>
2024-06-29 20:01:19 +0800

    Automated update of repository

--- server.js
+++ server.js
@@ -1,24 +1,22 @@
 const express = require('express');
 const path = require('path');
-const updateFlight = require('./api/update-flight');
-const flightProgress = require('./api/flight-progress');
 
 const app = express();
-
-app.use(express.json());
 
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Serve static files from the 'Image' directory
-app.use('/Image', express.static(path.join(__dirname, 'public', 'Image')));
+app.use('/Image', express.static(path.join(__dirname, 'public/Image')));
 
 // Serve static files from the 'data' directory
-app.use('/data', express.static(path.join(__dirname, 'public', 'data')));
+app.use('/data', express.static(path.join(__dirname, 'public/data')));
 
 // API Routes
+const updateFlight = require('./api/update-flight');
+const flightProgress = require('./api/flight-progress');
 app.use('/api/update-flight', updateFlight);
-app.use('/api', flightProgress);
+app.use('/api', flightProgress); // Combine both save-flight-state and saved-flight-state in one router
 
 // Define a route to serve your HTML file
 app.get('/', (req, res) => {
