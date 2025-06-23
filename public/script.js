cb3aa8dfa9cafb019f1c3159c638e6dfe74f812f
lucastng <lucastng@hotmail.com>
2025-06-23 21:07:48 +0800

    Automated update of repository

--- public/script.js
+++ public/script.js
@@ -152,7 +152,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 departure: flight.departure,
                 destination: flight.destination,
                 flightNumber: flight.flightNumber,
-                flightStatus: 'Completed',
+                flightStatus: '-',
                 image: flight.image
             });
             row.classList.add('static-flight');
