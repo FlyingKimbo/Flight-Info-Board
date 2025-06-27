611cd49e467e1e09974da33f2247ba4e8e260126
lucastng <lucastng@hotmail.com>
2025-06-27 17:57:23 +0800

    Automated update of repository

--- public/script.js
+++ public/script.js
@@ -6,8 +6,23 @@ const supabaseUrl = 'https://jwwaxqfckxmppsncvfbo.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp3d2F4cWZja3htcHBzbmN2ZmJvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA0MTY2MzUsImV4cCI6MjA2NTk5MjYzNX0.6fdsBgcAmjG9uwVbkyKhLW3sc7uCa1rwGj8aWBFgkFo'
 const supabase = createClient(supabaseUrl, supabaseKey)
 
+
+// When receiving 'Deboarding Completed':
+supabase
+    .channel('realtime')
+    .on('postgres_changes', { event: 'UPDATE', schema: 'public', table: 'flights_static' }, payload => {
+        if (payload.new.flightstatus === '-') {
+            document.getElementById('realtime-display').classList.add('empty-state');
+        }
+    })
+    .subscribe();
+
+
+
 // 1. CONSTANTS
 const POLLING_INTERVAL = 5000; // 1 second
+
+
 
 //Global Variable
 let GreenbarPercentage = 100
