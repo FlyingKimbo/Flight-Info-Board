440552a2b22a3ae0fbefa3c21d37e91ba5677c60
lucastng <lucastng@hotmail.com>
2025-06-27 18:48:36 +0800

    Automated update of repository

--- public/script.js
+++ public/script.js
@@ -6,15 +6,8 @@ const supabaseUrl = 'https://jwwaxqfckxmppsncvfbo.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp3d2F4cWZja3htcHBzbmN2ZmJvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA0MTY2MzUsImV4cCI6MjA2NTk5MjYzNX0.6fdsBgcAmjG9uwVbkyKhLW3sc7uCa1rwGj8aWBFgkFo'
 const supabase = createClient(supabaseUrl, supabaseKey)
 
-
-
-
-
-
 // 1. CONSTANTS
 const POLLING_INTERVAL = 5000; // 1 second
-
-
 
 //Global Variable
 let GreenbarPercentage = 100
