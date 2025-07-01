24635e9cc6f5557730595a6e07a1fe7da839e5d7
lucastng <lucastng@hotmail.com>
2025-07-01 17:21:03 +0800

    Automated update of repository

--- public/script.js
+++ public/script.js
@@ -294,7 +294,7 @@ function updateEteDist2ArrBar(flightData) {
         // Handle aircraft image - MODIFIED SECTION
         const aircraftType = flightData.current_flight?.split(' ')[0] || '';
         if (flightData.flight_status === "Deboarding Completed") {
-            
+            elements.eteBar.style.opacity = '0';
             // Special case for deboarding completed
             elements.aircraftImage.src = '/Image/Aircraft_Type/default_ground.png'; // Special "parked" image
             elements.aircraftImage.style.opacity = '1'; // Slightly transparent
