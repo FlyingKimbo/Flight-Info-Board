f94623ad01db676e284bfaa3aa27bebc012e533f
lucastng <lucastng@hotmail.com>
2025-07-01 16:19:23 +0800

    Automated update of repository

--- public/script.js
+++ public/script.js
@@ -131,7 +131,7 @@ async function updateFlightTable(staticData) {
 
 function updatePositions() {
     let Xoffset = 0;
-    let XoffsetFix = 355;
+    let XoffsetFix = 240;
 
 
     if (GreenbarPercentage >= 50) {
